e576010f6c8e4ee6667f8c24e7071f39f937dade
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2026-03-19 20:29:28 +0100

    add sanity

--- app/realisations/page.tsx
+++ app/realisations/page.tsx
@@ -1,4 +1,4 @@
-import MasonryGallery from "../src/components/realisations/MasonryGallery";
+
 
 export default function Materiel() {
   return (
@@ -13,7 +13,7 @@ export default function Materiel() {
 Découvrez nos réalisations de cabinets dentaires conçus sur mesure. Chaque projet allie design, ergonomie et respect des normes afin d’offrir un cadre de travail optimal aux praticiens et une expérience agréable aux patients.    </p>
         </div>
       </section>
-        <MasonryGallery />
+       
 </>
    
   );
