75bf7b554aebef2b708f5f06e574282e84e1d337
Cyrielle Thomas <cyriellethomas.pro@gmail.com>
2026-03-20 11:39:53 +0100

    config

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["cdn.sanity.io"],
   },
+  experimental: {
+    staleTimes: {
+      dynamic: 0,
+      static: 0,
+    },
+  },
 };
 
 export default nextConfig;
